680117eed7e2323f59fc7a7ccd710dc8bc83b175
ramis <axcelyw@gmail.com>
2022-09-19 16:43:06 +0500

    final

---
+++ script.js
@@ -0,0 +1,37 @@
+var str = prompt ('Введите своё имя:')
+var age = +prompt ('Введите свой возраст:')
+
+console.log(str);
+console.log(age);
+ 
+var solution = +prompt ('122 + 21 =')
+var solutio = +prompt ('122 - 21 =')
+var solutin = +prompt ('122 * 21 =')
+var soluton = +prompt ('122 / 21 =')
+var soluion = +prompt ('122 % 21 =')
+
+console.log('122 + 21 =' + (122 + 21)); 
+console.log('ваш ответ:' + solution);
+console.log('122 - 21 =' + (122 - 21));~
+console.log('ваш ответ:' + solutio);
+console.log('122 * 21 =' + (122 * 21));
+console.log('ваш ответ:' + solutin);
+console.log('122 / 21 =' + (122 / 21)); 
+console.log('ваш ответ:' + soluton);
+console.log('122 % 21 =' + (122 % 21));
+console.log('ваш ответ:' + soluion);
+
+
+
+
+var X = +prompt('Введите первое число:')
+var Y = +prompt('Введите второе число:')
+var Z = +prompt('Введите третье число:')
+
+var res = (X + Y + Z) / 3
+
+alert(res)
+console.log(X);
+console.log(Y);
+console.log(Z);
+console.log(res);
